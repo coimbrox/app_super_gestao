5e469818fd49c2042fbb27ed2ff1c39eed0e7e6f
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-07-29 14:53:48 -0300

    Blade - @empty

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,10 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        $fornecedores = ['1',];
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'off', 'cnpj' => '0',],
+            1 => ['nome' => 'Fornecedor 2', 'status' => 'on', 'cnpj' => ' 5051112181',]
+        ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,13 +3,15 @@
 @php
 
 @endphp
-
-{{-- @dd($fornecedores) --}}
-
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) >10 )
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda Não Existem Funcionários cadastrados</h3>
-@endif
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            VAZIO
+        @endempty
+    @endisset
+@endisset
