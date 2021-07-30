93a4a0bec18944dd1e97a743455e40fb17161b53
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-07-30 11:01:21 -0300

    Blade- Enviando Formulario de Contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,6 +9,7 @@ class ContatoController extends Controller
     //
     public function contato()
     {
+        var_dump($_GET);
         return view('site.contato', ['titulo' => 'Contato ']);
     }
 }
