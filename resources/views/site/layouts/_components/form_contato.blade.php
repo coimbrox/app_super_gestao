d1ab83bd5cb9e25bacd9d40af74e5b1a37028018
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-08-02 12:33:17 -0300

    Parametros por Componente

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,20 +1,21 @@
 {{ $slot }}
+
 <form action={{ route('site.contato') }} method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="borda-preta">
+    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
+    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
+    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
-    <select name="motivo_contato" class="borda-preta">
+    <select name="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         <option name="1" value="">Dúvida</option>
         <option name="2" value="">Elogio</option>
         <option name="3" value="">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
     <br>
-    <button type="submit" class="borda-preta">ENVIAR</button>
+    <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
